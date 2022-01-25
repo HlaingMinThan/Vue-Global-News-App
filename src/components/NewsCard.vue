5376ab119bf6dc1bc97e015a503aef9f83965efb
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2022-01-25 15:15:40 +0630

    Headline title Update Popup done

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -4,21 +4,31 @@
       height="250"
       src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
     ></v-img>
-    <v-card-title>Cafe Badilico</v-card-title>
+    <v-card-title class="d-flex justify-space-between"
+      ><div>Cafe Badilico</div>
+      <Dialog />
+    </v-card-title>
     <v-card-text>
       <div>
         <div class="text-subtitle-1 grey--text mb-2">20.6.2001</div>
         Small plates, salads & sandwiches - an intimate setting with 12 indoor
         seats plus patio seating.sandwiches - an intimate setting with 12 indoor
         seats plus patio seating.
-        <router-link :to="`/detail/${Math.random()}`">read more</router-link>
       </div>
     </v-card-text>
+    <div class="pa-4">
+      <router-link :to="`/detail/${Math.random()}`">
+        <v-btn color="primary" dark>Read More</v-btn>
+      </router-link>
+    </div>
   </v-card>
 </template>
 
 <script>
+import Dialog from "./Dialog.vue";
+
 export default {
+  components: { Dialog },
   data() {
     return {
       loading: false,
