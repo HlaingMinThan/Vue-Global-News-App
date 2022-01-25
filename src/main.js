dd9dab0a85b4d55889db93e302db8dec77ae46a9
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2022-01-25 15:19:45 +0630

    axios configuration done

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+
+// u can use axios like this in every component (this.axios)
+Vue.use(VueAxios, axios.create({ baseURL: "https://newsapi.org/v2" }));
 
 new Vue({
   router,
   store,
   vuetify,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
