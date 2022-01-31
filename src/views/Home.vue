307d0874f88137ccaf033260c55468d0f99a52d6
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2022-01-31 11:37:09 +0630

    added search feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,11 @@
     </div>
     <bread-crumbs />
     <v-row>
+      <v-col cols="4" class="mx-auto">
+        <v-text-field label="search articles" v-model="search"></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col
         v-for="(newHeadline, index) in newHeadlines"
         :key="index"
@@ -28,10 +33,23 @@ export default {
   data() {
     return {
       newHeadlines: [],
+      search: "",
     };
   },
+  watch: {
+    search() {
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        if (this.search.length) {
+          this.searchHeadlines();
+        } else {
+          this.getTopUsHeadlines();
+        }
+      }, 500);
+    },
+  },
   methods: {
-    async getHeadlines() {
+    async getTopUsHeadlines() {
       const res = await this.axios.get(
         "/top-headlines?country=us&apiKey=099148be22804e849a0c6fe022b7cf5e"
       );
@@ -39,9 +57,17 @@ export default {
       this.newHeadlines = res.data.articles;
       console.log(this.newHeadlines);
     },
+    async searchHeadlines() {
+      const res = await this.axios.get(
+        `/top-headlines?q=${this.search}&apiKey=099148be22804e849a0c6fe022b7cf5e`
+      );
+
+      this.newHeadlines = res.data.articles;
+      console.log(this.newHeadlines);
+    },
   },
   mounted() {
-    this.getHeadlines();
+    this.getTopUsHeadlines();
   },
 };
 </script>
